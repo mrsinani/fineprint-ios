0eccdf78e0c69f547b6b3c50b36bc5d160b9b1b1
Danaid Sinani <danaid@bu.edu>
2026-04-14 00:34:57 -0400

    app init

---
+++ Fineprint/Fineprint/FineprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  FineprintApp.swift
+//  Fineprint
+//
+//  Created by ⠀ xhexhi on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct FineprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
